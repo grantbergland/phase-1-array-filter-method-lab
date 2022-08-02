96a51453b947eb3f17ece9411bd357e14fa6efca
grantbergland <grantbergland@gmail.com>
2022-08-02 18:59:10 -0500

    commit tree 5394cb66d18a8489b7167d6f925fa1b09dda2373

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(arr, query) {
+    console.log(query)
+    return arr.filter(function(el) {
+      return el.toUpperCase().indexOf(query.toUpperCase()) !== -1
+    })
+  }
+
+  function fuzzyMatch(arr, query) {
+    return arr.filter(function(el) {
+        console.log('string',query.toLowerCase())
+      return el.toLowerCase().indexOf(query.toLowerCase()) === 0
+     // 'y' == -1
+    })
+    console.log('2',query.toLowerCase()) 
+  }
+function matchName(drivers, name){
+  return drivers.filter (function(el){
+    console.log(el)
+    if (name === el.name){
+    return el
+}}) 
+   }
